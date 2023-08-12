30cf388b6d93bc6ba99943e552d8c87f7a3f023d
ZHAO Xudong <zxdong@gmail.com>
2023-08-12 18:40:28 +0800

    Edit UI

--- bin/after.js
+++ bin/after.js
@@ -8,7 +8,7 @@ import { resolve } from 'path'
 async function build () {
   const fo = resolve(cwd, '../electerm')
   await exe(`cd ${fo} && git checkout gh-pages && cp -r ../electerm.html5beta.com/public/* ./ && git add --all && git commit -m 'update' && git push && git push gt gh-pages`)
-  await exe(`cd ${fo} && git fetch origin master:master && git push gt master:master`)
+  await exe(`cd ${fo} && git fetch origin master:master && git push gt master:master -f`)
 }
 
 build()
