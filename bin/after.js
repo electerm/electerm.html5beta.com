6377d690309eb4568ec199ac741a233ef29c74b9
ZHAO Xudong <zxdong@gmail.com>
2023-08-12 15:46:53 +0800

    fix

--- bin/after.js
+++ bin/after.js
@@ -7,7 +7,7 @@ import { resolve } from 'path'
 
 async function build () {
   const fo = resolve(cwd, '../electerm')
-  await exe(`cd ${fo} && git checkout gh-pages && cp -rf ../electerm.html5beta.com/data ./ && cp ../electerm.html5beta.com/public/*.html ./ && git add --all && git commit -m 'update' && git push && git push gt gh-pages`)
+  await exe(`cd ${fo} && git checkout gh-pages && cp -r ../electerm.html5beta.com/public/* ./ && git add --all && git commit -m 'update' && git push && git push gt gh-pages`)
   await exe(`cd ${fo} && git fetch origin master:master && git push gt master:master`)
 }
 
