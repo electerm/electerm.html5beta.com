92ca3dc61bcb84585ff9e8f716dfd5d3af0daba7
ZHAO Xudong <zxdong@gmail.com>
2024-01-12 18:46:53 +0800

    Update watch script

--- electerm.home.watch.js
+++ electerm.home.watch.js
@@ -6,11 +6,32 @@ import { cwd } from './bin/common.js'
 const filePath = resolve(cwd, 'data/electerm-github-release.json') // Replace with the actual path to the file
 const commandToRun = 'npm run b' // Replace with the command you want to run
 
+function checkVersion () {
+  try {
+    const d = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    const tag = d.release.name.toLowerCase()
+    return d.action === 'published' &&
+      !tag.includes('b') &&
+      !tag.includes('a') &&
+      d.release.assets &&
+      d.release.assets.length > 0 &&
+      d.release.body &&
+      d.release.body.length > 10
+  } catch (err) {
+    console.error('Failed to load file:', err)
+  }
+  return false
+}
+
 // Watch for changes in the file
 fs.watchFile(filePath, (curr, prev) => {
   if (curr.mtime !== prev.mtime) {
-    console.log('File has changed. Running command...')
-    executeCommand(commandToRun)
+    console.log(new Date(), 'File has changed. Running command...')
+    if (checkVersion()) {
+      executeCommand(commandToRun)
+    } else {
+      console.log('it is beta, no need to do anything')
+    }
   }
 })
 
